6e4ce48e1b336050a540617dfb76f6bbb80c1b0e
JorgeHFBueno <jorge.bueno@hotmail.com>
2025-05-09 08:32:02 -0300

    add(API edt)

--- api/src/routes/atividades.js
+++ api/src/routes/atividades.js
@@ -9,22 +9,23 @@ import {
 
 const r = Router();
 
-r.get('/', async (req, res) => {
-  // opcional: filtro por motorista ?motorista=email
-  const rows = await listAtividades(req.query.motorista);
-  res.json(rows);
-});
+r.get('/', async (_, res) => res.json(await listAtividades()));
 
 r.post('/', async (req, res) => {
   const nova = await createAtividade(req.body);
-  res.status(201).json(nova);
+  return res.status(201).json(nova);
 });
 
+// -------- rota por ID --------
 r.route('/:id')
-  .get(async (req, res) => res.json(await getAtividadeById(req.params.id)))
-  .put(async (req, res) =>
-    res.json(await updateAtividade(req.params.id, req.body))
-  )
+  .get(async (req, res) => {
+    const atv = await getAtividadeById(req.params.id);
+    atv ? res.json(atv) : res.sendStatus(404);
+  })
+  .put(async (req, res) => {
+    const upd = await updateAtividade(req.params.id, req.body);
+    upd ? res.json(upd) : res.sendStatus(404);
+  })
   .delete(async (req, res) => {
     await deleteAtividade(req.params.id);
     res.sendStatus(204);
